80a970b7d217ac9610edd7f12fe3cf3a8062068d
mybnae <piowm123@gmail.com>
2022-12-08 17:52:34 +0900

    123

--- assets/js/skill.js
+++ assets/js/skill.js
@@ -6,6 +6,7 @@ function mySkillEffect(){
     })
     document.querySelectorAll(".skSplit2")[38].insertAdjacentElement("afterend", document.createElement("br"));
     const skillIcon = gsap.utils.toArray(".skill_iconBox .icon .view");
+    const skillName = gsap.utils.toArray(".skill_iconBox .icon span");
 
     const skillTl = gsap.timeline({
         scrollTrigger: {
@@ -15,12 +16,22 @@ function mySkillEffect(){
             markers: true
         }
     })
+    
     skillTl.to(".skSplit1", {y: 0, opacity: 1, duration: 0.4, stagger: 0.05})
-    .to(".skSplit2", {y:0, opacity: 1, duration: 0.4, delay: -0.2, stagger: 0.01})
-    .to(skillIcon, {onUpdate:addFront, duration: 0.2, stagger: 0.1})
+    .to(".skSplit2", {y:0, opacity: 1, duration: 0.5, delay: -0.2, stagger: 0.003})
+    .to(skillIcon, {y: 0, opacity: 1, duration: 0.05, delay: -0.3, ease: "power3.out", stagger: 0.1}, "icon")
+    .to(skillName, {x: "50%", opacity: 1, duration: 0.5, delay: -0.3, ease: "power1.out", stagger: 0.1}, "icon")
+    .to(skillIcon, {rotateY: 0, duration: 0.1, delay: -0.1, stagger: 0.1, onUpdate: addFront})
     
 
     function addFront(){
-        
+        skillIcon.forEach((e)=>{
+            e.addEventListener("mouseenter",()=>{
+                e.style.transform = "rotateY(180deg)";
+            });
+            e.addEventListener("mouseleave",()=>{
+                e.style.transform = "rotateY(0deg)";
+            })
+        })
     }
 }
