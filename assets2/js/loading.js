d403d5caf25a54bd53e04216ef439a8307aeea94
mybnae <piowm123@gmail.com>
2022-12-29 11:40:54 +0900

    loading

--- assets2/js/loading.js
+++ assets2/js/loading.js
@@ -9,30 +9,96 @@ document.onreadystatechange = function() {
     // 로딩 후 실행
     } else if(document.readyState === "complete"){
         console.log("complete");
+
         const timer = setInterval(()=>{
-            if(document.querySelector(".loading-page .counter h1").innerText == "100%"){
-                clearInterval(timer);
-                setTimeout(complete, 800);
-            }
-        }, 100)
+            clearInterval(timer);
+            setTimeout(complete, 800);
+        }, 1000)
     }
 } 
 
 // 로딩 중 함수
 function loading(){
-    $(document).ready(function() {
-        var counter = 0;
-        var c = 0;
-        var i = setInterval(function(){
-            $(".loading-page .counter h1").html(c + "%");
-            $(".loading-page .counter .loading_bar").css("width", c + "%");
-            counter++;
-            c++;
-            
-            if(counter == 101) {
-                clearInterval(i);
+
+    function Ticker( elem ) {
+        elem.lettering();
+        this.done = false;
+        this.cycleCount = 5;
+        this.cycleCurrent = 0;
+        this.chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-_=+{}|[]\\;\':"<>?,./`~'.split('');
+        this.charsCount = this.chars.length;
+        this.letters = elem.find( 'span' );
+        this.letterCount = this.letters.length;
+        this.letterCurrent = 0;
+    
+        this.letters.each( function() {
+            var $this = $( this );
+            $this.attr( 'data-orig', $this.text() );
+            $this.text( '-' );
+        });
+    }
+    
+    Ticker.prototype.getChar = function() {
+        return this.chars[ Math.floor( Math.random() * this.charsCount ) ];
+    };
+    
+    Ticker.prototype.reset = function() {
+        this.done = false;
+        this.cycleCurrent = 0;
+        this.letterCurrent = 0;
+        this.letters.each( function() {
+            var $this = $( this );
+            $this.text( $this.attr( 'data-orig' ) );
+            $this.removeClass( 'done' );
+        });
+        this.loop();
+    };
+    
+    Ticker.prototype.loop = function() {
+        var self = this;
+    
+        this.letters.each( function( index, elem ) {
+            var $elem = $( elem );
+            if( index >= self.letterCurrent ) {
+                if( $elem.text() !== ' ' ) {
+                    $elem.text( self.getChar() );
+                    $elem.css( 'opacity', Math.random() );
+                }
             }
-        }, 0);
+        });
+    
+        if( this.cycleCurrent < this.cycleCount ) {
+            this.cycleCurrent++;
+        } else if( this.letterCurrent < this.letterCount ) {
+            var currLetter = this.letters.eq( this.letterCurrent );
+            this.cycleCurrent = 0;
+            currLetter.text( currLetter.attr( 'data-orig' ) ).css( 'opacity', 1 ).addClass( 'done' );
+            this.letterCurrent++;
+        } else {
+            this.done = true;
+        }
+    
+        if( !this.done ) {
+            requestAnimationFrame( function() {
+                self.loop();
+            });
+        } else if(document.readyState === "complete"){
+            setTimeout(()=>{
+                gsap.to(".word", {y: -70, opacity: 0, ease: "power3.out"} )
+            }, 300)
+        } else {
+            setTimeout( function() {
+                self.reset();
+            }, 750 );
+        } 
+    };
+    
+    $words = $( '.word' );
+    
+    $words.each( function() {
+        var $this = $( this ),
+            ticker = new Ticker( $this ).reset();
+        $this.data( 'ticker', ticker  );
     });
 }
 
